0f27d0eba89982b4e494b46cfd2b8e5e0bcd942b
Kamal-Ouiguemmane <ouiguemmanekamal@gmail.com>
2024-01-27 13:43:56 +0100

    writing the javascript code

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 const textInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const resultElement = document.getElementById('result');
+
+function onSubmit() {
+  const expression = textInput.value.trim();
+  if (expression === '') {
+    alert('Please enter some text.');
+    resultElement.textContent = '';
+    return;
+  }
+  const formattedExpression = removePunctuation(expression);
+  generateResultMessage(expression, isAPalindrome(expression));
+}
+
+function isAPalindrome(str) {
+  let lowerCasedStr = str.toLowerCase();
+  return lowerCasedStr === lowerCasedStr.split('').reverse().join('');
+}
+
+function removePunctuation(str) {
+  const punctuationString = ' !@#$%^&*()-=_+[]{}|;:\'",.<>/?` ';
+  return str
+    .split('')
+    .filter(el => !punctuationString.includes(el))
+    .join('');
+}
+
+function generateResultMessage(str, isPalindrome) {
+  resultElement.textContent = `${str} ${
+    isPalindrome ? 'is' : 'is not'
+  } a Palindrome`;
+}
+
+checkButton.addEventListener('click', onSubmit);
